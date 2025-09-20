38097464d7648dbd5439b192275b34ef9df28507
Anay Seth <anayseth@nic-4272.apng.wlan.private.upenn.edu>
2025-09-20 14:15:11 -0400

    Updated UI in UI.js and added functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "28471c8ecda85b4a"; }
+/******/ 		__webpack_require__.h = function() { return "ccc7f39d0697e515"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
